e09638aa32f768910ca796cc07a0101d67c7fedb
anmeth191 <anmeth191@gmail.com>
2018-08-20 08:07:56 -0600

    fixing the form making it more narrow from 90% to 50%

--- parallax.js
+++ parallax.js
@@ -1,8 +1,7 @@
 //get variables from the index to apply the parallax effect
 
 let showcase = document.querySelector('#showcase') , 
-showcase__content = document.querySelector('#showcase__content');
-
+showcase__content = document.querySelector('#showcase__content') ;
 
 //declare to window a event listener scroll and a function called parallaxEffect
 
@@ -13,6 +12,6 @@ function parallaxEffect(e){
 
 let scrollEvent = window.pageYOffset;
 
-showcase.style.top = (scrollEvent/2)+'px';
+showcase.style.top = Math.round(scrollEvent/2)+'px';
 showcase__content.style.top = 376-(scrollEvent * 0.1)+'px';
 }
